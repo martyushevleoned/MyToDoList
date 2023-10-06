0a393f8246d4baa791a5a11d4fa287a885367f55
Martyushev Leoned <56407820+martyushevleoned@users.noreply.github.com>
2023-10-06 15:22:14 +0500

    login page

--- MyToDoList/src/main/java/com/example/MyToDoList/config/WebSecurityConfig.java
+++ MyToDoList/src/main/java/com/example/MyToDoList/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/registration").permitAll()
+                        .requestMatchers("/", "/home", "/registration", "/static/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
